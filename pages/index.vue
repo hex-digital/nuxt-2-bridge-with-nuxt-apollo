6df769f0b975fed92619a338f30f3afa885f3b7c
Jamie Warburton <jamie@hexdigital.com>
2021-10-23 19:32:58 +0100

    Initial commit of Nuxt 2 with Bridge and Apollo

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ episodesByIds }}
+  </div>
+</template>
+<script>
+import episodes from '~/plugins/apollo/episodes.gql';
+
+export default {
+  apollo: {
+    episodesByIds: {
+      query: episodes,
+    },
+  },
+};
+</script>
---
+++ plugins/apollo/defaultClient.js
@@ -0,0 +1,6 @@
+// See: https://codesandbox.io/s/6ussn?file=/plugins/apollo/apolloConfig.js
+export default function () {
+  return {
+    httpEndpoint: 'https://rickandmortyapi.com/graphql',
+  }
+}
